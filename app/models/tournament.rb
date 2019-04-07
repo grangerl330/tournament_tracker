682f4c57b6da38d1eda749d41710d2418168ad89
grangerl330 <grangerl330@gmail.com>
2019-04-07 01:29:22 -0400

    refacored round_options_numbers method in tournament model

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -7,25 +7,10 @@ class Tournament < ApplicationRecord
   end
 
   def round_options_numbers
-    starting_round = self.draw_size
+    options = [256, 128, 64, 32, 16, 8, 4, 2]
+    starting_round = options.index(self.draw_size)
 
-    if starting_round == nil || starting_round == 256
-      [256, 128, 64, 32, 16, 8, 4, 2]
-    elsif starting_round == 128
-      [128, 64, 32, 16, 8, 4, 2]
-    elsif starting_round == 64
-      [64, 32, 16, 8, 4, 2]
-    elsif starting_round == 32
-      [32, 16, 8, 4, 2]
-    elsif starting_round == 16
-      [16, 8, 4, 2]
-    elsif starting_round == 8
-      [8, 4, 2]
-    elsif starting_round == 4
-      [4, 2]
-    elsif starting_round == 2
-      [2]
-    end
+    options[starting_round..-1]
   end
 
   def convert_round_options
