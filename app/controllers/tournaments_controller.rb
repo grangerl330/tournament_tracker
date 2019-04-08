2783511721760dd68f833825ee123f1298189119
grangerl330 <grangerl330@gmail.com>
2019-04-07 22:40:30 -0400

    added edit function to tournaments. added link to edit tournament on tournament show page

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,6 @@
 class TournamentsController < ApplicationController
   before_action :require_login
+  before_action :set_tournament, only: [:show, :edit, :update]
 
   def new
     @tournament = Tournament.new
@@ -13,13 +14,24 @@ class TournamentsController < ApplicationController
   end
 
   def show
-    @tournament = Tournament.find_by_id(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @tournament.update(tournament_params)
+    redirect_to tournament_path(@tournament)
   end
 
   private
 
   def tournament_params
     params.require(:tournament).permit(:title, :start_date, :end_date, :location, :draw_size, :points)
+  end
+
+  def set_tournament
+    @tournament = Tournament.find_by_id(params[:id])
   end
 
 end
