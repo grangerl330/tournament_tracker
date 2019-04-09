4e07e5c115b88190d73cfd1f035091e5bca3c538
grangerl330 <grangerl330@gmail.com>
2019-04-09 11:25:59 -0400

    added overall record method to user class that displays the user's total match wins and losses. added overall record to user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,23 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, uniqueness: true 
+  validates :email, uniqueness: true
 
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  def overall_record
+    wins = 0
+    losses = 0
+    self.matches.each do |match|
+      if match.won
+        wins += 1
+      else
+        losses += 1
+      end
+    end
+    "#{wins} - #{losses}"
+  end
+
 end
