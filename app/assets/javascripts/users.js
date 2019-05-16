66a25e34329b8cf1a2e003cd7732a41b41d6532e
grangerl330 <grangerl330@gmail.com>
2019-05-15 23:40:21 -0400

    added Tournament class constructor and tournamentHTML prototype to users.js.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,7 +13,34 @@ function listenForClick() {
       dataType: 'json'
     }).done(function(data){
       console.log("the data is: ", data)
-      
+
+      let tournament = new Tournament(data[0])
+      let tournamentHTML = tournament.tournamentHTML()
+
+      $('#show-tournaments').append(tournamentHTML)
     })
   })
+}
+
+class Tournament {
+  constructor(obj){
+    this.id = obj.id
+    this.title = obj.title
+    this.draw_size = obj.draw_size
+    this.location = obj.location
+    this.start_date = obj.start_date
+    this.end_date = obj.end_date
+    this.points = obj.points
+    this.won = obj.won
+    this.matches = obj.matches
+  }
+}
+
+Tournament.prototype.tournamentHTML = function() {
+  return(`
+
+    <h2>Tournaments</h2>
+
+    <h4>${this.title}</h4>
+    `)
 }
